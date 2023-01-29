fbeec0ca481396ca661dfce27f08c1ef64a4b6c6
kcnti <kantinun99998@gmail.com>
2023-01-29 21:43:48 +0700

    sample

---
+++ main.py
@@ -0,0 +1,22 @@
+import requests
+import os
+from bs4 import BeautifulSoup
+from utils.mycourseville import *
+from dotenv import load_dotenv
+
+load_dotenv()
+
+userid = os.environ.get('ID')
+password = os.environ.get('PASSWD')
+
+mcv = MCV(userid, password)
+session = mcv.login()
+
+assignments = mcv.getAssignments()
+print(assignments)
+
+courses = mcv.getCourses()
+print(courses)
+
+fileLinks = mcv.getFileLink()
+print(fileLinks)
