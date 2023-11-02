aedfe28c528248dcaaa4898bb2ff0b26cb099124
kcnti <kantinun99998@gmail.com>
2023-11-02 18:13:22 +0700

    download course materials

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import requests
 import os
-from bs4 import BeautifulSoup
 from utils.mycourseville import *
 from dotenv import load_dotenv
 
@@ -12,11 +10,39 @@ password = os.environ.get('PASSWD')
 mcv = MCV(userid, password)
 session = mcv.login()
 
-assignments = mcv.getAssignments()
-print(assignments)
+# assignments = mcv.getAssignments()
 
 # courses = mcv.getCourses()
-# print(courses)
 
-# fileLinks = mcv.getFileLink()
+fileLinks = mcv.getFileLink()
 # print(fileLinks)
+
+# Downloading Files
+def download(fileLinks, parentFolder):
+    if (not os.path.isdir(parentFolder)):
+        os.makedirs(parentFolder)
+
+    for subject in fileLinks:
+        print(subject.upper())
+        subFolder = parentFolder + '/' + subject
+        if (not os.path.isdir(subFolder)):
+            os.makedirs(subFolder)
+
+        for materialFolder in fileLinks[subject]:
+            matFolder = subFolder + '/' + materialFolder
+
+            for data in fileLinks[subject][materialFolder]:
+                extension = '.' + data['fileUrl'].split('.')[-1]
+                if (not os.path.isdir(matFolder)):
+                    os.makedirs(matFolder)
+                save = matFolder + '/' + data['fileName'] + extension
+                if (os.path.exists(save)):
+                    print(data['fileName'], 'exists')
+                    continue
+                dl = requests.get(data['fileUrl'], allow_redirects=True)
+                open(save, 'wb').write(dl.content)
+                print(data['fileName'], 'saved')
+
+# download(fileLinks, 'courseMaterials')
+
+
